6e5adb6f881f51927e303df731a95de60a82993e
Basil Homadi <deviltearbasil@gmail.com>
2020-09-18 12:52:41 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Add your functions here
+const map = function(srArr, srFunction){
+    let myArr= srArr.map(srFunction);
+    return myArr;
+}
+
+const reduce= function (srArr, srFunction, stPoint=0){
+    
+    if (typeof srArr.reduce(srFunction,stPoint)==='number'&& stPoint>0){
+        return srArr.reduce(srFunction,stPoint);
+    }
+}
